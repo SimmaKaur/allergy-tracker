3b6d489d2b1053dcd6feef197ac17752b22572a5
Simran Kaur <simrankaur@MacBook-Pro.local>
2024-06-18 18:30:26 +0530

    start v2

---
+++ extension files 2/content.js
@@ -0,0 +1,42 @@
+// content.js
+
+// Function to scrape and filter the list
+function scrapeAndFilterList() {
+    let allergens = ['Amerchol L101','Peg-16 lanolin','Lanolin peg-16','Peg-16 lanolin alcohol','Lanolin polyethylene glycol ether (16 moles)','Lanolin derivative'
+        ,'Lanolin PEG-16 acetate','Polyethylene glycol (16) lanolin','Lanolin PEG-16 ether'
+        ,'PEG-16 lanolin derivative','Lanolin ethoxylate (16 moles)'
+        ,'Benzoyl Peroxide','Bpo','Benzoyl peroxyde','Dibenzoyl peroxide','Peroxyde de benzoyle'
+        ,'Benzoyl superoxide','Dibenzoyl peroxide','Peroxybenzoyl','Peroxide, benzoyl'
+        ,'Benzoyl dioxide','Benzoyl hydroperoxide','Dibenzoyl peroxyde'
+        ,'Carba Mix','Carbamix','Carba mix II','Carbamide mix','Mercapto mix'
+        ,'Mercaptobenzothiazole mix','Mercaptobenzothiazole','MBT mix','MBT'
+        ,'Compositae Mix','Compositae plant mix','Asteraceae mix','Daisy mix'
+        ,'Ragweed mix','Composite mix','Chrysanthemum mix','Sunflower mix'
+        ,'Marigold mix','Arnica mix','D-Limonene','Limonene','(+)-Limonene'
+        ,'Limonene-D','Dipentene','p-Mentha-1,8-diene','Racemic limonene'
+        ,'dl-Limonene','Limoneno','Citrus terpenes','Decyl Glucoside'
+        ,'Decyl polyglucoside','Decyl d-glucoside','Alkyl polyglucoside'
+        ,'Polyglucoside','D-Glucopyranose','Decyl glucopyranoside'
+        ,'1-Decyl glucoside','Fragrance','Parfum','Perfume','Aroma','Essential oils'
+        ,'Fragrant oils','Scent','Odorant','Fragrance Mix','Perfume Mix'
+        ,'Fragrance cocktail','Scent mixture','Fragrance blend','Perfume blend'
+        ,'Glutaraldehyde','Glutaric dialdehyde','Glutaric aldehyde','Glutar'
+        ,'Glutaral','Glutaraldéhyde','Glutardialdehyde','Lidocaine','Xylocaine'
+        ,'Lignocaine','Lidocaine hydrochloride','Lidocaina','Lidocaína','Alphacaine'
+        ,'Lignocaine hydrochloride','Linalool','β-linalool','p-linalool'
+        ,'Linalyl alcohol','3,7-Dimethyl-1,6-octadien-3-ol'
+        ,'2,6-Dimethyl-2,7-octadien-6-ol','Propolis','Bee glue','Bee propolis'
+        ,'Bee cement','Propolis extract','Apis propolis','Beeswax'];
+    let ingredients = document.querySelectorAll('.css-26us6o');
+    let filteredItems = ingredients.filter(ingredient => allergens.includes(ingredient.toLowerCase()));
+
+    return filteredItems;
+}
+
+// Listen for messages from the extension popup
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action === 'scrapeAndFilterList') {
+        let filteredList = scrapeAndFilterList();
+        sendResponse({ filteredList: filteredList });
+    }
+});
